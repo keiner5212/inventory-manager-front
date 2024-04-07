838de59c2f20e1cfbbc243f4734780eab5405389
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-04-07 04:03:29 -0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Inventory_Management
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Inventory_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int contador = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OPA(Object sender, RoutedEventArgs e)
+        {
+            contador++;
+            Debug.WriteLine("Contador: ",contador);
+            Contador.Content="Opa: "+contador;
+
+        }
+    }
+}
